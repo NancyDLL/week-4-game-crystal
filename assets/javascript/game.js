ec498c2b3a625e8127bfcd6d740c069b486818f7
NancyDLL <nano14@mac.com>
2017-07-12 12:37:09 -0500

    psuedocoded the js.

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+//1 - Check connection to html page
+console.log("connected")
+
+//Psuedocode from the demo video
+//List of global variables
+	//wins
+	//losses
+//Start a round with:
+	//Generate a random number between 19 to 120 for matchNum
+	//Generate a random number between 1 to 12 for rubyNum
+	//Generate a random number between 1 to 12 for diamondNum
+	//Generate a random number between 1 to 12 for sapphireNum
+	//Generate a random number between 1 to 12 for emeraldNum
+	//yourScore is populated by the clicks on each gem image.
+	//youScore needs to be compared against the matchNum each time.
+	//if less than -> another click is allowed.
+//End a round with:
+	//else match than -> count win, reset matchNum and gems.
+	//else more than -> count loss, reset matchNum and gems and start a new round.
+//Considerations
+	//The variable for each gem would need to be held within the function for each gem.
+	//List of variables held within functions:
+		//matchNum
+		//rubyNum
+		//diamondNum
+		//sapphireNum
+		//emeraldNum
+		//userTotal
+
+
+
