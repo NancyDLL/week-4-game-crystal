eacccd6e8d8e4e7ed14475d69ec1c2e798d0ddea
NancyDLL <nano14@mac.com>
2017-07-12 12:54:23 -0500

    psuedocode for the function in the js.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,15 @@ console.log("connected")
 	//losses
 //Start a round with:
 	//Generate a random number between 19 to 120 for matchNum
-	//Generate a random number between 1 to 12 for rubyNum
-	//Generate a random number between 1 to 12 for diamondNum
-	//Generate a random number between 1 to 12 for sapphireNum
-	//Generate a random number between 1 to 12 for emeraldNum
+	//Generate a random number between 1 to 12 for rubyNum - diamondNum - sapphireNum - emeraldNum
+		//function numGenerator(){}
 	//yourScore is populated by the clicks on each gem image.
-	//youScore needs to be compared against the matchNum each time.
-	//if less than -> another click is allowed.
-//End a round with:
-	//else match than -> count win, reset matchNum and gems.
-	//else more than -> count loss, reset matchNum and gems and start a new round.
+		//function gemAdder(){}
+			//youScore needs to be compared against the matchNum each time.
+			//if less than -> another click is allowed.
+		//End function with:
+			//else match than -> count win, reset matchNum and gems.
+			//else more than -> count loss, reset matchNum and gems and start a new round.
 //Considerations
 	//The variable for each gem would need to be held within the function for each gem.
 	//List of variables held within functions:
@@ -25,7 +24,7 @@ console.log("connected")
 		//diamondNum
 		//sapphireNum
 		//emeraldNum
-		//userTotal
+		//yourScore
 
 
 
