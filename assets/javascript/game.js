2b78926562c26f4e844f226cf0a6df90e5be3d6e
NancyDLL <nano14@mac.com>
2017-07-12 19:00:09 -0500

    started to psuedocoded the JQuery stuff.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,25 +6,34 @@ console.log("connected")
 	//wins
 	//losses
 //Start a round with:
-	//Generate a random number between 19 to 120 for matchNum
+	//Generate a random number between 19 to 120 for matchNum variable.
+		//use JQuery to display matchNum in the ranNum div.
+				//matchNum = $('#ranNum');
 	//Generate a random number between 1 to 12 for rubyNum - diamondNum - sapphireNum - emeraldNum
 		//function numGenerator(){}
+			//These variables created within bigger function:
+				//rubyNum
+				//diamondNum
+				//sapphireNum
+				//emeraldNum
 	//yourScore is populated by the clicks on each gem image.
 		//function gemAdder(){}
-			//youScore needs to be compared against the matchNum each time.
+			//capture in the variable yourScore the value of the image clicked each time
+			//use JQuery to display yourScore in the yourScore div
+				//yourScore = $('yourScore');
+			//yourScore needs to be compared against the matchNum each time.
+			//This variable created by user clicks -> yourScore
 			//if less than -> another click is allowed.
 		//End function with:
 			//else match than -> count win, reset matchNum and gems.
+				//use JQuery to display win in the win div
+					//
 			//else more than -> count loss, reset matchNum and gems and start a new round.
+				//use JQuery to display loss in the loss div
+					//
 //Considerations
 	//The variable for each gem would need to be held within the function for each gem.
-	//List of variables held within functions:
-		//matchNum
-		//rubyNum
-		//diamondNum
-		//sapphireNum
-		//emeraldNum
-		//yourScore
+	
 
 
 
